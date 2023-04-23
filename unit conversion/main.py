9698208fc28d1be9960d34faebca6a14d104c034
seyed sina hosseini vaez <sshosseinivaez@gmail.com>
2023-04-23 21:24:05 +0330

    update

--- unit conversion/main.py
+++ unit conversion/main.py
@@ -134,12 +134,309 @@ class MainWindow(QMainWindow):
 
                 elif self.ui.to_box.currentText() == "Inch":
                     self.numericalـvalue = float(
-                        self.ui.input_box.text()) / ...
+                        self.ui.input_box.text()) * 0.04
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Centimeter":
+                if self.ui.to_box.currentText() == "Millimeter":
+                    self.numericalـvalue = float(self.ui.input_box.text()) * 10
+
+                elif self.ui.to_box.currentText() == "Centimeter":
+                    self.numericalـvalue = float(self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Meter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) / 100
+
+                elif self.ui.to_box.currentText() == "Kilometer":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) / 100000
+
+                elif self.ui.to_box.currentText() == "Inch":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.4
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Meter":
+                if self.ui.to_box.currentText() == "Millimeter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000
+
+                elif self.ui.to_box.currentText() == "Centimeter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 100
+
+                elif self.ui.to_box.currentText() == "Meter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Kilometer":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) / 1000
+
+                elif self.ui.to_box.currentText() == "Inch":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 39
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Kilometer":
+                if self.ui.to_box.currentText() == "Millimeter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000
+
+                elif self.ui.to_box.currentText() == "Centimeter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 100000
+
+                elif self.ui.to_box.currentText() == "Meter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000
+
+                elif self.ui.to_box.currentText() == "Kilometer":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Inch":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 39370
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Inch":
+                if self.ui.to_box.currentText() == "Millimeter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 25
+
+                elif self.ui.to_box.currentText() == "Centimeter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 2.54
+
+                elif self.ui.to_box.currentText() == "Meter":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.03
+
+                elif self.ui.to_box.currentText() == "Kilometer":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.00003
+
+                elif self.ui.to_box.currentText() == "Inch":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+        elif self.ui.convert_box.currentText() == "Temperature":
+            if self.ui.from_box.currentText() == "Celsius":
+                if self.ui.to_box.currentText() == "Celsius":
+                    self.numericalـvalue = float(self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Fahrenheit":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1.8 + 32
+
+                elif self.ui.to_box.currentText() == "Kelvin":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) + 274.15
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.to_box.currentText() == "Fahrenheit":
+                if self.ui.to_box.currentText() == "Celsius":
+                    self.numericalـvalue = float(self.ui.input_box.text())
+                    self.numericalـvalue = (self.numericalـvalue - 32) / 2
+
+                elif self.ui.to_box.currentText() == "Fahrenheit":
+                    self.numericalـvalue = float(self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Kelvin":
+                    self.numericalـvalue = float(self.ui.input_box.text())
+                    self.numericalـvalue = (self.numericalـvalue + 459.67) / 2
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.to_box.currentText() == "Kelvin":
+                if self.ui.to_box.currentText() == "Celsius":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) - 273.15
+
+                elif self.ui.to_box.currentText() == "Fahrenheit":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1.8 + 459.67
+
+                elif self.ui.to_box.currentText() == "Kelvin":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+        elif self.ui.convert_box.currentText() == "Digital volume":
+            if self.ui.from_box.currentText() == "bit":
+                if self.ui.to_box.currentText() == "bit":
+                    self.numericalـvalue = float(self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "byte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.125
+
+                elif self.ui.to_box.currentText() == "Kilobyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.001
+
+                elif self.ui.to_box.currentText() == "Megabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1.25e-7
+
+                elif self.ui.to_box.currentText() == "Gigabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1.25e-10
+
+                elif self.ui.to_box.currentText() == "Terabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1.25e-13
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "byte":
+                if self.ui.to_box.currentText() == "bit":
+                    self.numericalـvalue = float(self.ui.input_box.text()) * 8
+
+                elif self.ui.to_box.currentText() == "byte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Kilobyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.001
+
+                elif self.ui.to_box.currentText() == "Megabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.000001
+
+                elif self.ui.to_box.currentText() == "Gigabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1e-9
+
+                elif self.ui.to_box.currentText() == "Terabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1e-12
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Kilobyte":
+                if self.ui.to_box.currentText() == "bit":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 8000
+
+                elif self.ui.to_box.currentText() == "byte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000
+
+                elif self.ui.to_box.currentText() == "Kilobyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Megabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.001
+
+                elif self.ui.to_box.currentText() == "Gigabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.000001
+
+                elif self.ui.to_box.currentText() == "Terabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1e-9
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Megabyte":
+                if self.ui.to_box.currentText() == "bit":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 8000000
+
+                elif self.ui.to_box.currentText() == "byte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000
+
+                elif self.ui.to_box.currentText() == "Kilobyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000
+
+                elif self.ui.to_box.currentText() == "Megabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Gigabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.001
+
+                elif self.ui.to_box.currentText() == "Terabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.000001
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Gigabyte":
+                if self.ui.to_box.currentText() == "bit":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 8000000000
+
+                elif self.ui.to_box.currentText() == "byte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000000
+
+                elif self.ui.to_box.currentText() == "Kilobyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000
+
+                elif self.ui.to_box.currentText() == "Megabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000
+
+                elif self.ui.to_box.currentText() == "Gigabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
+
+                elif self.ui.to_box.currentText() == "Terabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 0.001
+
+                self.ui.output_box.setText(str(self.numericalـvalue))
+
+            elif self.ui.from_box.currentText() == "Terabyte":
+                if self.ui.to_box.currentText() == "bit":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 8000000000000
+
+                elif self.ui.to_box.currentText() == "byte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000000000
+
+                elif self.ui.to_box.currentText() == "Kilobyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000000
+
+                elif self.ui.to_box.currentText() == "Megabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000000
+
+                elif self.ui.to_box.currentText() == "Gigabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text()) * 1000
+
+                elif self.ui.to_box.currentText() == "Terabyte":
+                    self.numericalـvalue = float(
+                        self.ui.input_box.text())
 
                 self.ui.output_box.setText(str(self.numericalـvalue))
 
     def reset(self):
         self.ui.input_box.setText("")
+        self.ui.output_box.setText("")
+        self.numericalـvalue = ""
 
 
 if __name__ == "__main__":
